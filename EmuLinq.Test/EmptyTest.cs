ae4935337dfcc5d8a54a1a7fa7f8076457189386
ByteBlast <byteblast@gmail.com>
2014-06-14 10:58:29 +0100

    implemented empty.

---
+++ EmuLinq.Test/EmptyTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class EmptyTest
+    {
+        [Test]
+        public void EmptyContainsNoElements()
+        {
+            // act
+            // assert
+            CollectionAssert.IsEmpty(Enumerable.Empty<string>());
+        }
+
+        [Test]
+        public void EmptyIsCached()
+        {
+            // act 
+            // assert
+            Assert.AreSame(Enumerable.Empty<int>(), Enumerable.Empty<int>());
+            Assert.AreSame(Enumerable.Empty<string>(), Enumerable.Empty<string>());
+            Assert.AreSame(Enumerable.Empty<object>(), Enumerable.Empty<object>());
+            Assert.AreNotSame(Enumerable.Empty<long>(), Enumerable.Empty<int>());
+            Assert.AreNotSame(Enumerable.Empty<string>(), Enumerable.Empty<object>());
+        }
+    }
+}
---
+++ EmuLinq/Empty.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> Empty<TResult>()
+        {
+            return EmptyHolder<TResult>.Array;
+        }
+
+        private static class EmptyHolder<T>
+        {
+            internal static readonly T[] Array = new T[0];
+        }
+    }
+}
