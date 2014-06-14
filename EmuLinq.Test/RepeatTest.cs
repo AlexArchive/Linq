8d2bee39765e0f437fdd3c05a8205598f33b6148
ByteBlast <byteblast@gmail.com>
2014-06-14 11:11:43 +0100

    implemented repeat.

---
+++ EmuLinq.Test/RepeatTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class RepeatTest
+    {
+        [Test]
+        public void RepeatInteger()
+        {
+            // act
+            var result = Enumerable.Repeat(10, 2);
+
+            // assert
+            CollectionAssert.AreEquivalent(new[] { 10, 10 }, result);
+        }
+
+        [Test]
+        public void RepeatString()
+        {
+            // act
+            var result = Enumerable.Repeat("allo", 2);
+
+            // assert
+            CollectionAssert.AreEquivalent(new[] { "allo", "allo" }, result);
+        }
+
+        [Test]
+        public void RepeatZeroTimes()
+        {
+            CollectionAssert.IsEmpty(Enumerable.Repeat("", 0));
+        }
+
+        [Test]
+        public void ElementCanBeNull()
+        {
+            // act
+            var result = Enumerable.Repeat<string>(null, 2);
+
+            // assert
+            CollectionAssert.AreEquivalent(new string[] { null, null }, result);
+        }
+
+        [Test]
+        public void NegativeCountThrowsArgumentOutOfRangeException()
+        {
+            // act
+            // assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => Enumerable.Repeat("", -1));
+        }
+    }
+}
---
+++ EmuLinq/Repeat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> Repeat<TResult>(TResult element, int count)
+        {
+            if (count < 0)
+                throw new ArgumentOutOfRangeException("count");
+
+            return RepeatImpl(element, count);
+        }
+
+        private static IEnumerable<TResult> RepeatImpl<TResult>(TResult element, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return element;
+            }
+        }
+    }
+}
