6ead825e249724d5c732cd1e129d7ba94a042f46
ByteBlast <byteblast@gmail.com>
2014-06-16 07:40:34 +0100

    implemented longcount.

---
+++ EmuLinq.Test/LongCountTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class LongCountTest
+    {
+
+        [Test]
+        public void CountIsAccurate()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            var count = source.LongCount();
+
+            // assert
+            Assert.That(count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void EmptyCountIsAccurate()
+        {
+            // arrange
+            var source = new int[0];
+
+            // act
+            var count = source.LongCount();
+
+            Assert.That(count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void PredicatedCountIsAccurate()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            var count = source.LongCount(x => x != 2);
+
+            // assert
+            Assert.That(count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void PredicatedEmptyCountIsAccurate()
+        {
+            // arrange
+            var source = new int[0];
+
+            // act
+            var count = source.LongCount(x => x != 2);
+
+            Assert.That(count, Is.EqualTo(0));
+        }
+
+
+        [Test]
+        public void NullSourceThrowsArgumentNullException()
+        {
+            // arrange
+            IEnumerable<int> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.LongCount());
+        }
+
+        [Test]
+        public void PredicatedNullSourceThrowsArgumentNullException()
+        {
+            // arrange
+            IEnumerable<int> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.LongCount(x => x != 2));
+        }
+
+        [Test]
+        public void NullPredicateThrowsArgumentNullException()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.LongCount(null));
+        }
+
+    }
+}
---
+++ EmuLinq/LongCount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static long LongCount<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source ==null)
+                throw new ArgumentNullException();
+
+            long count = 0;
+            foreach (var element in source)
+            {
+                count++;
+            }
+            return count;
+        }
+
+        public static long LongCount<TSource>(this IEnumerable<TSource> source, 
+            Func<TSource, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException();
+
+            if (predicate == null)
+                throw new ArgumentNullException();
+
+            var filteredSource = source.Where(predicate);
+            return filteredSource.LongCount();
+        }
+    }
+}
