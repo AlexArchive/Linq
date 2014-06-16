f564d71cea32a4b9b7cb734c979f2809dc794829
ByteBlast <byteblast@gmail.com>
2014-06-16 17:40:18 +0100

    implemented select many.

---
+++ EmuLinq.Test/SelectManyTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class SelectManyTest
+    {
+        [Test]
+        public void SimpleFlatten()
+        {
+            // arrange
+            var sentences = new List<string> { "Hello World", "Goodbye World" };
+
+            // act
+            var result = sentences.SelectMany(n => n.Split(' '));
+
+            // assert
+            Assert.That(result.Count(), Is.EqualTo(4));
+
+            CollectionAssert.AreEquivalent(
+                new[] { "Hello", "World", "Goodbye", "World" }, result);
+        }
+
+        [Test]
+        public void SimpleFlattenWithIndex()
+        {
+            // arrange
+            var sentences = new List<string> { "Hello World", "Goodbye World" };
+
+            // act
+            var result = sentences.SelectMany((s, i) => s.Split(' ').Select(x => i + " " + x));
+
+            // assert
+            var expected = new[]
+            {
+                "0 Hello",
+                "0 World",
+                "1 Goodbye",
+                "1 World"
+            };
+            CollectionAssert.AreEquivalent(expected, result);
+        }
+    }
+}
---
+++ EmuLinq/SelectMany.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TResult>> selector)
+        {
+            foreach (var element in source)
+            {
+                var intermediates = selector(element);
+                foreach (var intermediate in intermediates)
+                {
+                    yield return intermediate;
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source,
+            Func<TSource, int, IEnumerable<TResult>> selector)
+        {
+            int index = 0;
+            foreach (var element in source)
+            {
+                var intermediates = selector(element, index);
+                foreach (var intermediate in intermediates)
+                {
+                    yield return intermediate;
+                }
+                index++;
+            }
+
+        }
+        
+    }
+}
