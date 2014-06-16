08526d22a01cd02126c9ec7890dc8fc62d122d59
ByteBlast <byteblast@gmail.com>
2014-06-16 07:56:48 +0100

    implemented concat.

---
+++ EmuLinq.Test/ConcatTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class ConcatTest
+    {
+        [Test]
+        public void ConactEnumerables()
+        {
+            // arrange
+            var first = new[] { 1, 2, 3 };
+            var second = new[] { 4, 5, 6 };
+
+            // act
+            var combined = first.Concat(second);
+
+            // assert
+            CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4, 5, 6 }, combined);
+        }
+
+        [Test]
+        public void NullFirstThrowsArgumentNullException()
+        {
+            // arrange
+            IEnumerable<int> first = null;
+            IEnumerable<int> second = new[] { 1 };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => first.Concat(second));
+        }
+
+        [Test]
+        public void NullSecondThrowsArgumentNullException()
+        {
+            // arrange
+            IEnumerable<int> first = new[] { 1 };
+            IEnumerable<int> second = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => first.Concat(second));
+        }
+    }
+}
---
+++ EmuLinq/Concat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> Concat<TSource>(this IEnumerable<TSource> first, 
+            IEnumerable<TSource> second)
+        {
+            if (first == null)
+                throw new ArgumentNullException("first");
+
+            if (second == null)
+                throw new ArgumentNullException("second");
+
+            return ConcatImpl(first, second);
+        }
+
+        private static IEnumerable<TSource> ConcatImpl<TSource>(this IEnumerable<TSource> first,
+            IEnumerable<TSource> second)
+        {
+            foreach (var element in first)
+            {
+                yield return element;
+            }
+
+            first = null;
+
+            foreach (var element in second)
+            {
+                yield return element;
+            }
+        }
+    }
+}
