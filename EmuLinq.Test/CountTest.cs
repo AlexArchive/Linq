e226b261959a95ec040e286aeee91027fb2319a0
ByteBlast <byteblast@gmail.com>
2014-06-16 07:29:22 +0100

    implemented count.

---
+++ EmuLinq.Test/CountTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class CountTest
+    {
+        [Test]
+        public void CountIsAccurate()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            var count = source.Count();
+
+            // assert
+            Assert.That(count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void EmptyCountIsAccurate()
+        {
+            // arrange
+            var source = new int[0];
+
+            // act
+            var count = source.Count();
+
+            Assert.That(count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void PredicatedCountIsAccurate()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            var count = source.Count(x => x != 2);
+
+            // assert
+            Assert.That(count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void PredicatedEmptyCountIsAccurate()
+        {
+            // arrange
+            var source = new int[0];
+
+            // act
+            var count = source.Count(x => x != 2);
+
+            Assert.That(count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void NullSourceThrowsArgumentNullException()
+        {
+            // arrange
+            IEnumerable<int> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.Count());
+        }
+
+        [Test]
+        public void PredicatedNullSourceThrowsArgumentNullException()
+        {
+            // arrange
+            IEnumerable<int> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.Count(x => x != 2));
+        }
+
+        [Test]
+        public void NullPredicateThrowsArgumentNullException()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.Count(null));
+        }
+    }
+}
---
+++ EmuLinq/Count.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial  class Enumerable
+    {
+        public static int Count<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            var count = 0;
+            foreach (var element in source)
+            {
+                count++;
+                // test this..
+                if (count == Int32.MaxValue)
+                    throw new OverflowException();
+            }
+            return count;
+        }
+
+        public static int Count<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            var filteredSource = source.Where(predicate);
+            return filteredSource.Count();
+        }
+
+        // use ICollection.Count when applicable for better performance.
+    }
+}
