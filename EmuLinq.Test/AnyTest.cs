58fed3fba605f8aae7d605da9f27da1bfdcebc14
ByteBlast <byteblast@gmail.com>
2014-06-17 18:49:38 +0100

    implemented any.

---
+++ EmuLinq.Test/AnyTest.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class AnyTest
+    {
+        [Test]
+        public void EmptySourceReturnsFalse()
+        {
+            // arrange
+            var source = new int[0];
+
+            // act
+            // assert
+            Assert.That(source.Any(), Is.False);
+        }
+
+        [Test]
+        public void NonEmptySourceReturnsTrue()
+        {
+            // arrange
+            var source = new[] { 1 };
+
+            // act
+            // assert
+            Assert.That(source.Any(), Is.True);
+        }
+
+        [Test]
+        public void NullSourceThrowsArgumentNullException()
+        {
+            // arrange
+            IEnumerable<int> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.Any());
+        }
+
+        [Test]
+        public void EmptySourceWithPredicateReturnsFalse()
+        {
+            // arrange
+            var source = new int[0];
+
+            // act
+            // assert
+            Assert.That(source.Any(x => x == 1), Is.False);
+        }
+
+        [Test]
+        public void NonEmptySourceWithMatchingPredicateReturnsTrue()
+        {
+            // arrange
+            var source = new[] { 1 };
+
+            // act
+            // assert
+            Assert.That(source.Any(x => x == 1), Is.True);
+        }
+        [Test]
+        public void NonEmptySourceWithNonMatchingPredicateReturnsFalse()
+        {
+            // arrange
+            var source = new[] { 1 };
+
+            // act
+            // assert
+            Assert.That(source.Any(x => x == 3), Is.False);
+        }
+
+        [Test]
+        public void NullSourceWithPredicateThrowsArgumentNullException()
+        {
+            // arrange
+            IEnumerable<int> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.Any(x => x == 1));
+        }
+
+        [Test]
+        public void NullPredicateThrowsArgumentNullException()
+        {
+            // arrange
+            var source = new[] { 1 };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.Any(null));
+        }
+    }
+}
---
+++ EmuLinq/Any.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static bool Any<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            return source.GetEnumerator().MoveNext();
+        }
+
+        public static bool Any<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            foreach (var element in source)
+            {
+                if (predicate(element))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
