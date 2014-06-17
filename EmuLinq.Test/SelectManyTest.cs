95a7c21b256c7815ef0ead08c3c28d5358de9e6b
ByteBlast <byteblast@gmail.com>
2014-06-17 15:35:09 +0100

    implemented select many overloads.

--- EmuLinq.Test/SelectManyTest.cs
+++ EmuLinq.Test/SelectManyTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace EmuLinq.Test
@@ -10,36 +11,194 @@ namespace EmuLinq.Test
         public void SimpleFlatten()
         {
             // arrange
-            var sentences = new List<string> { "Hello World", "Goodbye World" };
+            var source = new [] { "Mexico Brazil", "Australia Netherlands" };
 
             // act
-            var result = sentences.SelectMany(n => n.Split(' '));
+            var result = source.SelectMany(group => group.Split(' '));
 
             // assert
-            Assert.That(result.Count(), Is.EqualTo(4));
-
             CollectionAssert.AreEquivalent(
-                new[] { "Hello", "World", "Goodbye", "World" }, result);
+                new[] { "Mexico", "Brazil", "Australia", "Netherlands" }, result);
         }
 
         [Test]
         public void SimpleFlattenWithIndex()
         {
             // arrange
-            var sentences = new List<string> { "Hello World", "Goodbye World" };
+            var source = new[] { "Mexico Brazil", "Australia Netherlands" };
 
             // act
-            var result = sentences.SelectMany((s, i) => s.Split(' ').Select(x => i + " " + x));
+            var result = source.SelectMany(
+                (group, groupIndex) => group.Split(' ').Select(country => groupIndex + " " + country));
 
             // assert
             var expected = new[]
             {
-                "0 Hello",
-                "0 World",
-                "1 Goodbye",
-                "1 World"
+                "0 Mexico",
+                "0 Brazil",
+                "1 Australia",
+                "1 Netherlands"
             };
             CollectionAssert.AreEquivalent(expected, result);
+        }
+
+        [Test]
+        public void FlattenWithProjection()
+        {
+            // arrange
+            var source = new[] { "Mexico Brazil", "Australia Netherlands" };
+
+            // act
+            var result =
+                source.SelectMany(
+                    group => group.Split(' '), (group, country) => group + " > " + country);
+
+            // assert
+            CollectionAssert.AreEquivalent(new[] 
+            {
+                "Mexico Brazil > Mexico",
+                "Mexico Brazil > Brazil",
+                "Australia Netherlands > Australia",
+                "Australia Netherlands > Netherlands"
+            }, result);
+        }
+
+        [Test]
+        public void FlattenWithProjectionAndIndex()
+        {
+            // arrange
+            var source = new[] { "Mexico Brazil", "Australia Netherlands" };
+
+            // act
+            var result =
+                source.SelectMany(
+                    (group, index) => group.Split(' ').Select(country => index + " " + country),
+                    (group, country) => group + " > " + country);
+
+            // assert
+            CollectionAssert.AreEquivalent(new[]
+            {
+                "Mexico Brazil > 0 Mexico",
+                "Mexico Brazil > 0 Brazil",
+                "Australia Netherlands > 1 Australia",
+                "Australia Netherlands > 1 Netherlands"
+            }, result);
+        }
+
+        [Test]
+        public void NullSourceThrowsArgumentNullException()
+        {
+            // arrange
+            IEnumerable<string> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany(s => s));
+        }
+
+        [Test]
+        public void NullSelectorThrowsArgumentNullException()
+        {
+            // arrange
+            var source = new[] { "Mexico Brazil", "Australia Netherlands" };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() =>
+                source.SelectMany((Func<string, IEnumerable<char>>) null));
+        }
+
+        [Test]
+        public void WithIndexNullSourceThrowsArgumentNullException()
+        {
+            // arrange
+            IEnumerable<string> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany((s, index) => s));
+        }
+
+        [Test]
+        public void WithIndexNullSelectorThrowsArgumentNullException()
+        {
+            // arrange
+            var source = new[] { "Mexico Brazil", "Australia Netherlands" };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() =>
+                source.SelectMany((Func<string, int, IEnumerable<char>>) null));
+        }
+
+        [Test]
+        public void WithProjectionNullSourceThrowsArgumentNullException()
+        {
+            // arrange
+            IEnumerable<string> source = null;
+            
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany(s => s, (s, c) => c));
+        }
+
+        [Test]
+        public void WithProjectionNullCollectionSelectorThrowsArgumentNullException()
+        {
+            // arrange
+            var source = new[] { "Mexico Brazil", "Australia Netherlands" };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() =>
+                source.SelectMany((Func<string, IEnumerable<char>>) null, (s, c) => c));
+        }
+
+        [Test]
+        public void WithProjectionNullResultCollectionSelectorThrowsArgumentNullException()
+        {
+            // arrange
+            var source = new[] { "Mexico Brazil", "Australia Netherlands" };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() =>
+                source.SelectMany(s => s, null));
+        }
+
+        [Test]
+        public void WithProjectionAndIndexNullSourceThrowsArgumentNullException()
+        {
+            // arrange
+            IEnumerable<string> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany((s, index) => s, (s, c) => c));
+        }
+
+        [Test]
+        public void WithProjectionAndIndexNullCollectionSelectorThrowsArgumentNullException()
+        {
+            // arrange
+            var source = new[] { "Mexico Brazil", "Australia Netherlands" };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() =>
+                source.SelectMany((Func<string, int, IEnumerable<char>>)null, (s, c) => c));
+        }
+
+        [Test]
+        public void WithProjectionAndIndexNullResultSelectorThrowsArgumentNullException()
+        {
+            // act
+            var source = new[] { "Mexico Brazil", "Australia Netherlands" };
+
+            // arrange
+            // assert
+            Assert.Throws<ArgumentNullException>(() =>
+                source.SelectMany((s, i) => s, null));
         }
     }
 }
--- EmuLinq/SelectMany.cs
+++ EmuLinq/SelectMany.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 
 namespace EmuLinq
@@ -8,12 +8,23 @@ namespace EmuLinq
         public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source,
             Func<TSource, IEnumerable<TResult>> selector)
         {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (selector == null)
+                throw new ArgumentNullException("selector");
+
+            return SelectManyImpl(source, selector);
+        }
+
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TResult>> selector)
+        {
             foreach (var element in source)
             {
-                var intermediates = selector(element);
-                foreach (var intermediate in intermediates)
+                foreach (var subElement in selector(element))
                 {
-                    yield return intermediate;
+                    yield return subElement;
                 }
             }
         }
@@ -21,18 +32,86 @@ namespace EmuLinq
         public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source,
             Func<TSource, int, IEnumerable<TResult>> selector)
         {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (selector== null)
+                throw new ArgumentNullException("selector");
+
+            return SelectManyImpl(source, selector);
+        }
+
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(IEnumerable<TSource> source,
+            Func<TSource, int, IEnumerable<TResult>> selector)
+        {
             int index = 0;
             foreach (var element in source)
             {
-                var intermediates = selector(element, index);
-                foreach (var intermediate in intermediates)
+                foreach (var subElement in selector(element, index))
                 {
-                    yield return intermediate;
+                    yield return subElement;
                 }
                 index++;
             }
-
         }
-        
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TResult>> collectionSelector,
+            Func<TSource, TResult, TResult> resultSelector)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (collectionSelector==null)
+                throw new ArgumentNullException("collectionSelector");    
+
+            if (resultSelector==null)
+                throw new ArgumentNullException("resultSelector");
+
+            return SelectManyImpl(source, collectionSelector, resultSelector);
+        }
+
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TResult>> collectionSelector,
+            Func<TSource, TResult, TResult> resultSelector)
+        {
+            foreach (var element in source)
+            {
+                foreach (var subElement in collectionSelector(element))
+                {
+                    yield return resultSelector(element, subElement);
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source,
+            Func<TSource, int, IEnumerable<TResult>> collectionSelector,
+            Func<TSource, TResult, TResult> resultSelector)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (collectionSelector == null)
+                throw new ArgumentNullException("collectionSelector");
+
+            if (resultSelector==null)
+                throw new ArgumentNullException("resultSelector");
+
+            return SelectManyImpl(source, collectionSelector, resultSelector);
+        }
+
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(IEnumerable<TSource> source,
+            Func<TSource, int, IEnumerable<TResult>> collectionSelector, 
+            Func<TSource, TResult, TResult> resultSelector)
+        {
+            int index = 0;
+            foreach (var element in source)
+            {
+                foreach (var subElement in collectionSelector(element, index))
+                {
+                    yield return resultSelector(element, subElement);
+                }
+                index++;
+            }
+        }
     }
 }
