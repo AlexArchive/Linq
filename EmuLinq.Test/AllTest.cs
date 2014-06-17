d61b2bb0e490189fee8e2fcf0d09bc3a374e2dc5
ByteBlast <byteblast@gmail.com>
2014-06-17 19:03:22 +0100

    implemented all.

---
+++ EmuLinq.Test/AllTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class AllTest
+    {
+
+        [Test]
+        public void PredicateMachingAllElementsReturnsTrue()
+        {
+            var source = new[] { "Alex", "Andrew", "Anders", "Andrea" };
+            Assert.That(source.All(x => x.StartsWith("A")), Is.True);
+        }
+
+        [Test]
+        public void PredicateMatchingNoElementsReturnsFalse()
+        {
+            var source = new[] { "Alex", "Andrew", "Anders", "Andrea" };
+            Assert.That(source.All(x => x.StartsWith("B")), Is.False);
+        }
+
+        [Test]
+        public void PredicateMatchingSomeElementsReturnsFalse()
+        {
+            var source = new[] { "Alex", "Andrew", "Anders", "Andrea", "Bob" };
+            Assert.That(source.All(x => x.StartsWith("B")), Is.False);
+        }
+
+        [Test]
+        public void EmptySourceReturnsTrue()
+        {
+            var source = new int[0];
+            Assert.That(source.All(x => x == 10), Is.True);
+        }
+
+        [Test]
+        public void NullPredicateThrowsArgumentNullException()
+        {
+            // arrange
+            var source = new[] { 1 };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.All(null));
+        }
+
+        [Test]
+        public void NullSourceWithPredicateThrowsArgumentNullException()
+        {
+            // arrange
+            IEnumerable<int> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.All(x => x == 1));
+        }
+    }
+}
---
+++ EmuLinq/All.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable  
+    {
+        public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            foreach (var element in source)
+            {
+                if (!predicate(element))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
