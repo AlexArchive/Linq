5e0f31f6f44d000f40424bddbad24f5dbe98be09
ByteBlast <byteblast@gmail.com>
2014-06-18 07:31:44 +0100

    implemented first.

---
+++ EmuLinq.Test/FirstTest.cs
@@ -0,0 +1,142 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        [Test]
+        public void EmptySequenceWithoutPredicate()
+        {
+            // arrange
+            var source = new int[] { };
+
+            // act
+            // assert
+            Assert.Throws<InvalidOperationException>(() => source.First());
+        }
+
+        [Test]
+        public void SingleElementSequenceWithoutPredicate()
+        {
+            // arrange
+            var source = new[] { 1 };
+
+            // act
+            // assert
+            Assert.That(source.First(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void MultipleElementSequenceWithoutPredicate()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            // assert
+            Assert.That(source.First(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void NullSourceWithoutPredicate()
+        {
+            // arrange
+            IEnumerable<int> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.First());
+        }
+
+        [Test]
+        public void EmptySequenceWithPredicate()
+        {
+            // arrange
+            var source = new int[] { };
+
+            // act
+            // assert
+            Assert.Throws<InvalidOperationException>(() => source.First(x => true));
+        }
+
+        [Test]
+        public void SingleElementSequenceWithMatchingPredicate()
+        {
+            // arrange
+            var source = new[] { 1 };
+
+            // act
+            // assert
+            Assert.That(source.First(n => n % 2 != 0), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SingleElementSequenceWithNonMatchingPredicate()
+        {
+            // arrange
+            var source = new[] { 2 };
+
+            // act
+            // assert
+            Assert.Throws<InvalidOperationException>(() => source.First(n => n % 2 != 0));
+        }
+
+        [Test]
+        public void MultipleElementSequenceWithSinglePredicateMatch()
+        {
+            // arrange
+            var source = new[] { 1 };
+
+            // act
+            // assert
+            Assert.That(source.First(n => n % 2 != 0), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void MultipleElementSequenceWithMultiplePredicateMatches()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            // assert
+            Assert.That(source.First(n => n % 2 != 0), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void MultipleElementSequenceWithNonMatchingPreicate()
+        {
+            // arrange
+            var source = new[] { 0, 2, 4 };
+
+            // act
+            // assert
+            Assert.Throws<InvalidOperationException>(() => source.First(n => n % 2 != 0));
+        }
+
+        [Test]
+        public void NullSourceWithPredicate()
+        {
+            // arrange
+            IEnumerable<int> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.First(x => true));
+        }
+
+        [Test]
+        public void NullPredicate()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.First(null));
+        }
+    }
+}
---
+++ EmuLinq/First.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static TSource First<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            foreach (var element in source)
+            {
+                return element;
+            }
+
+            throw new InvalidOperationException();
+        }
+
+        public static TSource First<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            foreach (var element in source)
+            {
+                if (predicate(element))
+                    return element;
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
