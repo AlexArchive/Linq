8ae5824ad7ca04837b243d2d396cdd110b458120
ByteBlast <byteblast@gmail.com>
2014-06-19 20:36:16 +0100

    implemented single or default.

---
+++ EmuLinq.Test/SingleOrDefaultTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class SingleOrDefaultTest
+    {
+        [Test]
+        public void NullSourceWithoutPredicate()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.SingleOrDefault());
+        }
+
+        [Test]
+        public void EmptySequenceWithoutPredicate()
+        {
+            var source = new int[] { };
+            Assert.That(source.SingleOrDefault(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SingleElementSequenceWithoutPredicate()
+        {
+            var source = new[] { 1 };
+            Assert.That(source.SingleOrDefault(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void MultipleElementSequenceWithoutPredicate()
+        {
+            var source = new[] { 1, 2, 3 };
+            Assert.That(source.SingleOrDefault(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void NullSourceWithPredicate()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.SingleOrDefault(x => true));
+        }
+
+        [Test]
+        public void EmptySequenceWithPredicate()
+        {
+            var source = new int[] { };
+            Assert.That(source.SingleOrDefault(x => true), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SingleElementSequenceWithMatchingPredicate()
+        {
+            var source = new[] { 1 };
+            Assert.That(source.SingleOrDefault(x => x == 1), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SingleElementSequenceWithNonMatchingPredicate()
+        {
+            var source = new[] { 1 };
+            Assert.That(source.SingleOrDefault(x => x == 2), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void MultipleElementSequenceWithSinglePredicateMatch()
+        {
+            var source = new[] { 1, 2, 3 };
+            Assert.That(source.SingleOrDefault(x => x==1), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void MultipleElementSequenceWithMultiplePredicateMatches()
+        {
+            var source = new[] { 1, 2, 3, 1 };
+            Assert.That(source.SingleOrDefault(x => x == 1), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void MultipleElementSequenceWithNonMatchingPredicate()
+        {
+            var source = new[] { 1, 2, 3 };
+            Assert.That(source.SingleOrDefault(x => x == 10), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void NullPredicate()
+        {
+            var source = new[] { 1, 2, 3 };
+            Assert.Throws<ArgumentNullException>(() => source.SingleOrDefault(null));
+        }
+    }
+}
---
+++ EmuLinq/SingleOrDefault.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmuLinq
+{
+
+    public static partial class Enumerable
+    {
+        public static TSource SingleOrDefault<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            var enumerator = source.GetEnumerator();
+
+            if (!enumerator.MoveNext())
+            {
+                return default(TSource);
+            }
+
+            var single = enumerator.Current;
+
+            if (enumerator.MoveNext())
+            {
+                return default(TSource);
+            }
+
+            return single;
+        }
+
+        public static TSource SingleOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            var enumerator = source.GetEnumerator();
+
+            if (!enumerator.MoveNext())
+            {
+                return default(TSource);
+
+            }
+
+            var single = enumerator.Current;
+
+            if (!predicate(single))
+                return default(TSource);
+
+            if (enumerator.MoveNext())
+            {
+                return default(TSource);
+
+            }
+
+            return single;
+        }
+    }
+
+}
