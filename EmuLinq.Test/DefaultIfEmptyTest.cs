b893cd54296a4bc7fab0c6119d871d4f79ac5d66
ByteBlast <byteblast@gmail.com>
2014-06-20 08:35:35 +0100

    implemented DefaultIfEmpty.

---
+++ EmuLinq.Test/DefaultIfEmptyTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class DefaultIfEmptyTest
+    {
+        [Test]
+        public void NullSource()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.DefaultIfEmpty());
+        }
+
+        [Test]
+        public void EmptySequence()
+        {
+            var source = new string[] { };
+            CollectionAssert.AreEquivalent(new string[] { null }, source.DefaultIfEmpty());
+        }
+
+        [Test]
+        public void NonEmptySequence()
+        {
+            var source = new[] { 1, 2, 3 };
+            CollectionAssert.AreEquivalent(source, source.DefaultIfEmpty());
+        }
+        
+        [Test]
+        public void NullSourceWithSpecifiedDefaultValue()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.DefaultIfEmpty(5));
+        }
+        
+        [Test]
+        public void EmptySequenceWithDefaultValuet()
+        {
+            var source = new string[] { };
+            CollectionAssert.AreEquivalent(new[] { "England" }, source.DefaultIfEmpty("England"));
+        }
+
+        [Test]
+        public void NonEmptySequenceWithDefaultValue()
+        {
+            var source = new[] { 1, 2, 3 };
+            CollectionAssert.AreEquivalent(source, source.DefaultIfEmpty(4));
+        }
+    }
+}
---
+++ EmuLinq/DefaultIfEmpty.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> DefaultIfEmpty<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source==null)
+                throw new ArgumentNullException("source");
+
+            var enumerator = source.GetEnumerator();
+            if (enumerator.MoveNext())
+            {
+                return source;
+            }
+
+            return new[] { default( TSource ) };
+        }
+
+        public static IEnumerable<TSource> DefaultIfEmpty<TSource>(this IEnumerable<TSource> source, TSource defaultValue)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+
+            var enumerator = source.GetEnumerator();
+            if (enumerator.MoveNext())
+            {
+                return source;
+            }
+
+            return new[] { defaultValue };
+        }
+
+    }
+}
