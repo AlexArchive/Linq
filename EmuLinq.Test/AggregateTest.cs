c76e375da8282f336af937da6c99dd34591f4fdd
ByteBlast <byteblast@gmail.com>
2014-06-21 23:36:24 +0100

    implemented Aggregate.

---
+++ EmuLinq.Test/AggregateTest.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class AggregateTest
+    {
+        [Test]
+        public void NullSource()
+        {
+            // arrange
+            IEnumerable<int> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.Aggregate((a, b) => a));
+        }
+
+        [Test]
+        public void NullFunc()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.Aggregate(null));
+        }
+
+        [Test]
+        public void UnseededAggregation()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // act
+            var result = source.Aggregate((sum, number) => sum + number);
+
+            // assert
+            Assert.That(result, Is.EqualTo(55));
+        }
+
+        [Test]
+        public void UnseededAggregationWithEmptySequence()
+        {
+            // arrange
+            var source = new int[] { };
+
+            // act
+            // assert
+            Assert.Throws<InvalidOperationException>(() => source.Aggregate((sum, number) => sum + number));
+        }
+
+        [Test]
+        public void UnseededAggregationUsesFirstElementOfSequenceAsSeed()
+        {
+            // arrange
+            int[] source = { 5, 3, 2 };
+
+            // act
+            // assert
+            Assert.AreEqual(30, source.Aggregate((acc, value) => acc * value));
+        }
+
+        [Test]
+        public void NullSourceSeeded()
+        {
+            // arrange
+            IEnumerable<int> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.Aggregate(0, (a, b) => a));
+        }
+
+        [Test]
+        public void NullFuncSeeded()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.Aggregate(0, null));
+        }
+
+        [Test]
+        public void SeededAggregation()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // act
+            var result = source.Aggregate(10, (sum, number) => sum + number);
+
+            // assert
+            Assert.That(result, Is.EqualTo(65));
+        }
+
+        [Test]
+        public void NullSourceSeededWithResultSelector()
+        {
+            // arrange
+            IEnumerable<int> source = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.Aggregate(0, (a, b) => a, a => a));
+        }
+
+        [Test]
+        public void NullFuncSeededWithResultSelector()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.Aggregate(0, null, i => i));
+        }
+
+        [Test]
+        public void NullResultSelector()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => source.Aggregate(0, (a, b) => a, (Func<int, int>)null));
+        }
+
+        [Test]
+        public void SeededAggregationWithResultSelector()
+        {
+            // arrange
+            var source = new[] { 1, 2, 3 };
+
+            // act
+            var result = source.Aggregate(
+                new { Sum = 0, Count = 0 },
+                (anonObj, number) => new { Sum = anonObj.Sum + number, Count = anonObj.Count + 1 },
+                anonObj => anonObj.Sum / anonObj.Count);
+
+            // assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
---
+++ EmuLinq/Aggregate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static TSource Aggregate<TSource>(this IEnumerable<TSource> source, 
+            Func<TSource, TSource, TSource> func)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (func == null) throw new ArgumentNullException("func");
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                if (!enumerator.MoveNext()) throw new InvalidOperationException();
+
+                TSource result = enumerator.Current;
+
+                while (enumerator.MoveNext())
+                {
+                    result = func(result, enumerator.Current);
+                }
+
+                return result;
+            }
+        }
+
+        public static TAccumulate Aggregate<TSource, TAccumulate>(this IEnumerable<TSource> source,
+            TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> func)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (func == null) throw new ArgumentNullException("func");
+
+            foreach (var element in source)
+            {
+                seed = func(seed, element);
+            }
+
+            return seed;
+        }
+
+        public static TResult Aggregate<TSource, TAccumulate, TResult>(
+            this IEnumerable<TSource> source,
+            TAccumulate seed, 
+            Func<TAccumulate, TSource, TAccumulate> func,
+            Func<TAccumulate, TResult> resultSelector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (func == null) throw new ArgumentNullException("func");
+            if (resultSelector == null) throw new ArgumentNullException("resultSelector");
+
+            foreach (var element in source)
+            {
+                seed = func(seed, element);
+            }
+
+            return resultSelector(seed);
+        }
+    }
+}
