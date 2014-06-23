14533a1ceb005a529472aa4b024b7197a3155aac
ByteBlast <byteblast@gmail.com>
2014-06-23 21:24:20 +0100

    implemented Union.

---
+++ EmuLinq.Test/UnionTest.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class UnionTest
+    {
+        [Test]
+        public void UnionWithoutComparer()
+        {
+            // arrange
+            var first = new[] { "a", "b", "c" };
+            var second = new[] { "b", "c", "d", "e", "f" };
+
+            // act
+            var result = first.Union(second);
+
+            // assert
+            CollectionAssert.AreEquivalent(new[] { "a", "b", "c", "d", "e", "f" }, result);
+        }
+
+        [Test]
+        public void UnionWithNullComparer()
+        {
+            // arrange
+            var first = new[] { "a", "b", "c" };
+            var second = new[] { "b", "c", "d", "e", "f" };
+
+            // act
+            var result = first.Union(second, null);
+
+            // assert
+            CollectionAssert.AreEquivalent(new[] { "a", "b", "c", "d", "e", "f" }, result);
+        }
+
+        [Test]
+        public void UnionWithCaseInsensitiveComparer()
+        {
+            // arrange
+            var first = new[] { "a", "b", "c" };
+            var second = new[] { "B", "C", "d", "e", "f" };
+
+            // act
+            var result = first.Union(second, StringComparer.OrdinalIgnoreCase);
+
+            // assert
+            CollectionAssert.AreEquivalent(new[] { "a", "b", "c", "d", "e", "f" }, result);
+        }
+
+        [Test]
+        public void UnionWithEmptyFirstSequence()
+        {
+            // arrange
+            var first = new string[] { };
+            var second = new[] { "a", "a", "b", "c" };
+            
+            // act
+            var result = first.Union(second);
+
+            // assert
+            CollectionAssert.AreEquivalent(new[] { "a", "b", "c" }, result);
+        }
+
+        [Test]
+        public void UnionWithEmptySecondSequence()
+        {
+            // arrange
+            var first = new[] { "a", "a", "b", "c" };
+            var second = new string[] { };
+
+            // act
+            var result = first.Union(second);
+
+            // assert
+            CollectionAssert.AreEquivalent(new[] { "a", "b", "c" }, result);
+        }
+
+        [Test]
+        public void UnionWithTwoEmptySequences()
+        {
+            // arrange
+            string[] first = { };
+            string[] second = { };
+
+            // act
+            // assert
+            CollectionAssert.IsEmpty(first.Union(second));
+        }
+
+        [Test]
+        public void NullFirstWithoutComparer()
+        {
+            // arrange
+            IEnumerable<string> first = null;
+            var second = new string[] { };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => first.Union(second));
+        }
+
+        [Test]
+        public void NullSecondWithoutComparer()
+        {
+            // arrange
+            var first = new string[] { };
+            IEnumerable<string> second = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => first.Union(second));
+        }
+
+        [Test]
+        public void NullFirstWithComparer()
+        {
+            // arrange
+            IEnumerable<string> first = null;
+            var second = new string[] { };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => first.Union(second, StringComparer.Ordinal));
+        }
+
+        [Test]
+        public void NullSecondWithComparer()
+        {
+            // arrange
+            var first = new string[] { };
+            IEnumerable<string> second = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => first.Union(second, StringComparer.Ordinal));
+        }
+    }
+}
---
+++ EmuLinq/Union.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> Union<TSource>(this IEnumerable<TSource> first, 
+            IEnumerable<TSource> second)
+        {
+            return Union(first, second, EqualityComparer<TSource>.Default);
+        }
+        
+        public static IEnumerable<TSource> Union<TSource>(this IEnumerable<TSource> first,
+            IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
+        {
+            if (first == null)
+            {
+                throw new ArgumentNullException("first");
+            }
+
+            if (second == null)
+            {
+                throw new ArgumentNullException("second");
+            }
+
+            return UnionIterator(first, second, comparer ?? EqualityComparer<TSource>.Default);
+        }
+
+        private static IEnumerable<TSource> UnionIterator<TSource>(IEnumerable<TSource> first,
+            IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
+        {
+            var streamedElements = new HashSet<TSource>(comparer);
+
+            foreach (var element in first)
+            {
+                if (streamedElements.Add(element))
+                {
+                    yield return element;
+                }
+            }
+
+            foreach (var element in second)
+            {
+                if (streamedElements.Add(element))
+                {
+                    yield return element;
+                }
+            }
+        }
+    }
+}
