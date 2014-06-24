47b9bc78d1b83c51c58e91379f4c80b9cb1e8e98
ByteBlast <byteblast@gmail.com>
2014-06-24 16:46:10 +0100

    implemented Except.

---
+++ EmuLinq.Test/ExceptTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class ExceptTest
+    {
+        [Test]
+        public void ExceptWithoutComparer()
+        {
+            // arrange
+            var first = new[] { "a", "b", "c", "d", "e" };
+            var second = new[] { "a", "b", "c" };
+
+            // act
+            var result = first.Except(second);
+
+            // assert
+            CollectionAssert.AreEquivalent(new[] { "d", "e" }, result);
+        }
+
+        [Test]
+        public void ExceptWithNullComparer()
+        {
+            // arrange
+            var first = new[] { "a", "b", "c", "d", "e" };
+            var second = new[] { "a", "b", "c" };
+
+            // act
+            var result = first.Except(second, null);
+
+            // assert
+            CollectionAssert.AreEquivalent(new[] { "d", "e" }, result);
+        }
+
+        [Test]
+        public void ExceptWithCaseInsensitiveComparer()
+        {
+            // arrange
+            var first = new[] { "a", "B", "c", "d", "e" };
+            var second = new[] { "a", "b", "C" };
+
+            // act
+            var result = first.Except(second, StringComparer.OrdinalIgnoreCase);
+
+            // assert
+            CollectionAssert.AreEquivalent(new[] { "d", "e" }, result);
+        }
+
+        [Test]
+        public void NullFirstWithoutComparer()
+        {
+            // arrange
+            IEnumerable<string> first = null;
+            var second = new string[] { };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => first.Except(second));
+        }
+
+        [Test]
+        public void NullSecondWithoutComparer()
+        {
+            // arrange
+            var first = new string[] { };
+            IEnumerable<string> second = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => first.Except(second));
+        }
+
+        [Test]
+        public void NullFirstWithComparer()
+        {
+            // arrange
+            IEnumerable<string> first = null;
+            var second = new string[] { };
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => first.Except(second, StringComparer.Ordinal));
+        }
+
+        [Test]
+        public void NullSecondWithComparer()
+        {
+            // arrange
+            var first = new string[] { };
+            IEnumerable<string> second = null;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentNullException>(() => first.Except(second, StringComparer.Ordinal));
+        }
+
+    }
+}
---
+++ EmuLinq/Except.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> Except<TSource>(
+            this IEnumerable<TSource> first,
+            IEnumerable<TSource> second)
+        {
+            return Except(first, second, null);
+        }
+
+        public static IEnumerable<TSource> Except<TSource>(
+            this IEnumerable<TSource> first,
+            IEnumerable<TSource> second,
+            IEqualityComparer<TSource> comparer)
+        {
+            Ensure.IsNotNull(first, "first");
+            Ensure.IsNotNull(second, "second");
+
+            return ExceptIterator(first, second, comparer ?? EqualityComparer<TSource>.Default);
+        }
+
+        private static IEnumerable<TSource> ExceptIterator<TSource>(
+            this IEnumerable<TSource> first,
+            IEnumerable<TSource> second,
+            IEqualityComparer<TSource> comparer)
+        {
+            var streamedElements = new HashSet<TSource>(second, comparer);
+            foreach (var element in first) {
+                if (streamedElements.Add(element)) {
+                    yield return element;
+                }
+            }
+        } 
+    }
+}
