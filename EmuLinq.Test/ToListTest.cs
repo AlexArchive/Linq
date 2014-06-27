85b1155edac1ad01e421b959c1ccf68c89c89b2b
ByteBlast <byteblast@gmail.com>
2014-06-27 23:41:56 +0100

    implemented ToList.

---
+++ EmuLinq.Test/ToListTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class ToListTest
+    {
+        [Test]
+        public void SimpleSequence()
+        {
+            var source = Enumerable.Range(0, 100);
+            var result = source.ToList();
+            CollectionAssert.AreEquivalent(source, result);
+        }
+
+        [Test]
+        public void NullSource()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() =>source.ToList());
+        }
+    }
+}
---
+++ EmuLinq/ToList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static List<TSource> ToList<TSource>(
+            this IEnumerable<TSource> source)
+        {
+            Ensure.IsNotNull(source, "source");
+
+            return new List<TSource>(source);
+        }
+    }
+}
