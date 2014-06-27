8e93998424f1ac4a33f2565265c4b096639e135c
ByteBlast <byteblast@gmail.com>
2014-06-27 23:32:17 +0100

    implemented ToArray.

---
+++ EmuLinq.Test/ToArrayTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class ToArrayTest
+    {
+        [Test]
+        public void SimpleSequence()
+        {
+            IEnumerable<int> source = Enumerable.Range(0, 12);
+            int[] result = source.ToArray();
+            CollectionAssert.AreEquivalent(source, result);
+        }
+
+        [Test]
+        public void NullSource()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.ToArray());
+        }
+    }
+}
---
+++ EmuLinq/ToArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static TSource[] ToArray<TSource>(
+            this IEnumerable<TSource> source)
+        {
+            Ensure.IsNotNull(source, "source");
+
+            TSource[] array = new TSource[5];
+            int count = 0;
+            foreach (var element in source) {
+                if (array.Length == count) {
+                    Array.Resize(ref array, count * 2);
+                }
+                array[count] = element;
+                count++;
+            }
+            Array.Resize(ref array, count);
+            return array;
+        }
+    }
+}
