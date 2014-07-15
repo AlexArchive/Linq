f895757dbc5af5d397a202af46505b64a274f847
ByteBlast <byteblast@gmail.com>
2014-07-15 21:31:52 +0100

    implemented Take.

---
+++ EmuLinq.Test/TakeTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class TakeTest
+    {
+        [Test]
+        public void NullSource_Throws()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.Take(10));
+        }
+
+        [Test]
+        public void NegativeCount_ReturnsEmptySequence()
+        {
+            var sequence = Enumerable.Range(0, 100);
+            var actual = sequence.Take(-100);
+            CollectionAssert.IsEmpty(actual);
+        }
+
+        [Test]
+        public void OnlyEnumerateTheGivenNumberOfElements()
+        {
+            var sequence = new[] { 1, 2, 0 };
+            var query = sequence.Select(x => 10 / x);
+            var actual = query.Take(2);
+            CollectionAssert.AreEquivalent(new[] { 10, 5 }, actual);
+        }
+
+        [Test]
+        public void CountShorterThanSource()
+        {
+            var sequence = Enumerable.Range(1, 10);
+            var actual = sequence.Take(5);
+            CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4, 5 }, actual);
+        }
+
+        [Test]
+        public void CountEqualToSource()
+        {
+            var sequence = Enumerable.Range(1, 10);
+            var actual = sequence.Take(10);
+            CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, actual);
+        }
+
+        [Test]
+        public void CountGreaterThanSource()
+        {
+            var sequence = Enumerable.Range(1, 10);
+            var actual = sequence.Take(15);
+            CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, actual);
+        }
+    }
+}
---
+++ EmuLinq/Take.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> Take<TSource>(
+            this IEnumerable<TSource> source, 
+            int count)
+        {
+            Ensure.IsNotNull(source, "source");
+            return TakeIterator(source, count);
+        }
+
+        private static IEnumerable<TSource> TakeIterator<TSource>(
+            IEnumerable<TSource> source, 
+            int count)
+        {
+            int elementsStreamed = 0;
+            using (var enumerator = source.GetEnumerator()) {
+                while (enumerator.MoveNext()) {
+                    elementsStreamed += 1;
+                    yield return enumerator.Current;
+                    if (elementsStreamed == count) yield break;
+                }
+            }
+        }
+    }
+}
