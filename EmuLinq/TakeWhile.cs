7f3541b540ffa596ddddd37d914596798f20f478
ByteBlast <byteblast@gmail.com>
2014-07-16 11:51:28 +0100

    implemented TakeWhile.

---
+++ EmuLinq/TakeWhile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> TakeWhile<TSource>(
+            this IEnumerable<TSource> source, 
+            Func<TSource, bool> predicate)
+        {
+            Ensure.IsNotNull(source, "source");
+            Ensure.IsNotNull(predicate, "predicate");
+            return TakeWhileIterator(source, predicate);
+        }
+
+        private static IEnumerable<TSource> TakeWhileIterator<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, int, bool> predicate)
+        {
+            int index = 0;
+            foreach (var element in source) {
+                if (!predicate(element, index)) break;
+                yield return element;
+                index += 1;
+            }
+        }
+
+        private static IEnumerable<TSource> TakeWhileIterator<TSource>(
+            IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            foreach (var element in source) {
+                if (!predicate(element)) break;
+                yield return element;
+            }
+        }
+
+        public static IEnumerable<TSource> TakeWhile<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, int, bool> predicate)
+        {
+            Ensure.IsNotNull(source, "source");
+            Ensure.IsNotNull(predicate, "predicate");
+            return TakeWhileIterator(source, predicate);
+        }
+    }
+}
