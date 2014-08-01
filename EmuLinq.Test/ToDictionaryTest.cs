477add645721007c87d4576467ca0e5e6a899dc4
ByteBlast <byteblast@gmail.com>
2014-08-01 04:38:03 +0100

    implemented ToDictionary.

---
+++ EmuLinq.Test/ToDictionaryTest.cs
@@ -0,0 +1,163 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace EmuLinq.Test
+{
+    [TestFixture]
+    public class ToDictionaryTest
+    {
+        [Test]
+        public void NullSource_KeySelector()
+        {
+            IEnumerable<int> source = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.ToDictionary(element => element));
+        }
+
+        [Test]
+        public void NullSelector_KeySelector()
+        {
+            IEnumerable<int> source = Enumerable.Empty<int>();
+            Func<int, int> selector = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.ToDictionary(selector));
+        }
+
+        [Test]
+        public void KeySelector()
+        {
+            var source = new[] { "stack", "exchange" };
+
+            var actual = source.ToDictionary(element => element[0]);
+
+            Assert.That(actual['s'], Is.EqualTo("stack"));
+            Assert.That(actual['e'], Is.EqualTo("exchange"));
+        }
+
+        [Test]
+        public void NullSource_KeySelectorWithComparer()
+        {
+            IEnumerable<string> source = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+                source.ToDictionary(element => element, StringComparer.OrdinalIgnoreCase));
+        }
+
+        [Test]
+        public void NullSelector_KeySelectorWithComparer()
+        {
+            IEnumerable<string> source = Enumerable.Empty<string>();
+
+            Assert.Throws<ArgumentNullException>(() =>
+                source.ToDictionary(null, StringComparer.OrdinalIgnoreCase));
+        }
+
+        [Test]
+        public void KeySelectorWithComparer()
+        {
+            var source = new[] { "stack", "exchange", "Stack" };
+
+            Assert.Throws<ArgumentException>(() =>
+                source.ToDictionary(element => element, StringComparer.OrdinalIgnoreCase));
+        }
+
+        [Test]
+        public void KeySelectorWithNullComparer()
+        {
+            var source = new[] { "stack", "exchange", "Stack" };
+            IEqualityComparer<char> comparer = null;
+
+            var actual = source.ToDictionary(element => element[0], comparer);
+
+            Assert.That(actual['s'], Is.EqualTo("stack"));
+            Assert.That(actual['S'], Is.EqualTo("Stack"));
+            Assert.That(actual['e'], Is.EqualTo("exchange"));
+        }
+
+        [Test]
+        public void NullSource_KeyAndElementSelector()
+        {
+            IEnumerable<int> source = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.ToDictionary(element => element, element => element));
+        }
+
+        [Test]
+        public void NullKeySelector_KeyAndElementSelector()
+        {
+            var source = Enumerable.Empty<int>();
+            Func<int, int> keySelector = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.ToDictionary(keySelector, element => element));
+        }
+
+        [Test]
+        public void NullElementSelector_KeyAndElementSelector()
+        {
+            var source = Enumerable.Empty<int>();
+            Func<int, int> elementSelector = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.ToDictionary(element => element, elementSelector));
+        }
+
+        [Test]
+        public void KeyAndElementSelector()
+        {
+            var source = new[] { "stack", "exchange" };
+
+            var actual = source.ToDictionary(element => element[0], element => element.Length);
+
+            Assert.That(actual['s'], Is.EqualTo(5));
+            Assert.That(actual['e'], Is.EqualTo(8));
+        }
+
+        [Test]
+        public void NullSource_KeyAndElementSelectorWithComparer()
+        {
+            IEnumerable<string> source = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.ToDictionary(element => element, element => element, StringComparer.OrdinalIgnoreCase));
+        }
+        
+        [Test]
+        public void NullKeySelector_KeyAndElementSelectorWithComparer()
+        {
+            IEnumerable<string> source = Enumerable.Empty<string>();
+            Func<string, string> keySelector = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.ToDictionary(keySelector, element => element, StringComparer.OrdinalIgnoreCase));
+        }
+     
+        [Test]
+        public void NullElementSelector_KeyAndElementSelectorWithComparer()
+        {
+            IEnumerable<string> source = Enumerable.Empty<string>();
+            Func<string, string> elementSelector = null;
+
+            Assert.Throws<ArgumentNullException>(() => source.ToDictionary(element => element,elementSelector, StringComparer.OrdinalIgnoreCase));
+        }
+
+        [Test]
+        public void KeyAndElementSelectorWithComparer()
+        {
+            var source = new[] { "stack", "exchange", "Stack" };
+
+            Assert.Throws<ArgumentException>(() =>
+                source.ToDictionary(element => element, element => element, StringComparer.OrdinalIgnoreCase));
+        }
+
+        [Test]
+        public void KeyAndElementSelectorWithNullComparer()
+        {
+            var source = new[] { "stack", "exchange", "Stack" };
+            IEqualityComparer<char> comparer = null;
+
+            var actual = source.ToDictionary(element => element[0], element => element.Length, comparer);
+
+            Assert.That(actual['s'], Is.EqualTo(5));
+            Assert.That(actual['S'], Is.EqualTo(5));
+            Assert.That(actual['e'], Is.EqualTo(8));
+        }
+    }
+}
---
+++ EmuLinq/ToDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace EmuLinq
+{
+    public static partial class Enumerable
+    {
+        public static IDictionary<TKey, TSource> ToDictionary<TKey, TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            return source.ToDictionary(keySelector, null);
+        }
+
+        public static IDictionary<TKey, TSource> ToDictionary<TKey, TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            IEqualityComparer<TKey> comparer) 
+        {
+            return source.ToDictionary(keySelector, element => element, comparer);
+        }
+
+        public static IDictionary<TKey, TElement> ToDictionary<TKey, TSource, TElement>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            Func<TSource, TElement> elementSelector)
+        {
+            return source.ToDictionary(keySelector, elementSelector, null);
+        }
+
+        public static IDictionary<TKey, TElement> ToDictionary<TKey, TSource, TElement>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            Func<TSource, TElement> elementSelector,
+            IEqualityComparer<TKey> comparer)
+        {
+            Ensure.IsNotNull(source, "source");
+            Ensure.IsNotNull(keySelector, "keySelector");
+            Ensure.IsNotNull(elementSelector, "elementSelector");
+
+            var dictionary = new Dictionary<TKey, TElement>(comparer);
+            foreach (var element in source) {
+                dictionary.Add(keySelector(element), elementSelector(element));
+            }
+            return dictionary;
+        }
+    }
+}
